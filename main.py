46f2be4c43c61e343414d317cd6fd62b763df83b
stushor <business.stushor.jap@gmail.com>
2024-09-28 17:18:13 +0500

    Update

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from PIL import Image
 
-image = Image.open('monro.jpg')
-print(image.mode) 
+image = Image.open("monro.jpg")
 red, green, blue = image.split()
 
-new_image = Image.merge("RGB", (red, green, blue))
-red_coordinates = (50, 0, red.width, red.height)
-blue_coordinates = (-50, 0, blue.width, blue.height)
-red_cropped = new_image.crop(red_coordinates)
+red_coordinates1 = (101, 0, red.width, red.height)
+red_coordinates2 = (50, 0, 645, red.height)
+blue_coordinates1 = (50, 0, 645, blue.height)
+blue_coordinates2 = (0, 0, 595, blue.height)
+green_coordinates = (50, 0, 645, green.height)
+
+red_left = red.crop(red_coordinates1)
+red_middle = red.crop(red_coordinates2)
+complete_red = Image.blend(red_left, red_middle, 0.5)
+
+blue_middle = blue.crop(blue_coordinates1)
+blue_right = blue.crop(blue_coordinates2)
+complete_blue = Image.blend(blue_middle, blue_right, 0.5)
+complete_green = green.crop(green_coordinates)
+
+complete_monro = Image.merge('RGB', (complete_red, complete_green, complete_blue))
+complete_monro.save('complete_monro.jpg')
+
+monro_image = Image.open("complete_monro.jpg")
+monro_image.thumbnail((80, 80))
+monro_image.save('monro_avatar.jpg')
